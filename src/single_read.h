42b9c7d53412a88ddecd8fedc5eaf6f1d2903457
darxsys <darxsys@gmail.com>
2015-05-27 12:37:16 +0200

    argh

---
+++ src/single_read.h
@@ -0,0 +1,19 @@
+#ifndef SINGLE_READ_H
+#define SINGLE_READ_H
+
+namespace isomorph {
+	struct SingleRead : public Read {
+		public:
+		int id;
+		// full id from fastq file
+		seqan::CharString fa_id;
+		seqan::Dna5String seq;
+		seqan::CharString phred;
+		
+		// all the transcript alignments that are good
+		// transcript id, left_position, right_position
+		std::vector<std::tuple<int, int> > pi_x_n;				
+	}
+}
+
+#endif // SINGLE_READ_H
